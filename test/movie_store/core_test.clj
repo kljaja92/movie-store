28c6d30fc7709c08bbd04a434d80b50a56578af0
John Doe <johndoe@email.com>
2024-08-21 20:35:14 +0200

    Tenth commit

--- test/movie_store/core_test.clj
+++ test/movie_store/core_test.clj
@@ -1,6 +1,10 @@
 (ns movie-store.core-test
   (:require [clojure.test :refer :all]
-            [movie-store.core :refer :all]))
+            [movie-store.core :refer :all]
+            [movie-store.domain.actors :refer :all]
+            [movie-store.domain.producers :refer :all]
+            [movie-store.domain.cinemas :refer :all]
+            [midje.sweet :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
